7876955dabdc908321e333a3d57ba5376ac2c2da
Eman Bayani <emanbayani@yahoo.com>
2018-07-11 14:31:45 -0500

    Add files via upload

--- public/src/js/config.js
+++ public/src/js/config.js
@@ -5,7 +5,7 @@ angular.module('insight').config(function($routeProvider) {
   $routeProvider.
     when('/block/:blockHash', {
       templateUrl: '/views/block.html',
-      title: 'Litecoin Block '
+      title: 'Pixo Cash Block '
     }).
     when('/block-index/:blockHeight', {
       controller: 'BlocksController',
@@ -13,7 +13,7 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/tx/:txId/:v_type?/:v_index?', {
       templateUrl: '/views/transaction.html',
-      title: 'Litecoin Transaction '
+      title: 'Pixo Cash Transaction '
     }).
     when('/', {
       templateUrl: '/views/index.html',
@@ -21,15 +21,15 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/blocks', {
       templateUrl: '/views/block_list.html',
-      title: 'Litecoin Blocks solved Today'
+      title: 'Pixo Cash Blocks solved Today'
     }).
     when('/blocks-date/:blockDate/:startTimestamp?', {
       templateUrl: '/views/block_list.html',
-      title: 'Litecoin Blocks solved '
+      title: 'Pixo Cash Blocks solved '
     }).
     when('/address/:addrStr', {
       templateUrl: '/views/address.html',
-      title: 'Litecoin Address '
+      title: 'Pixo Cash Address '
     }).
     when('/status', {
       templateUrl: '/views/status.html',
